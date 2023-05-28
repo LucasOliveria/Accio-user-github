ce5905e753e8f740846a7df7b8786cd8167e946e
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-05-28 11:45:06 -0300

    Pagina de usuários em andamento

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -4,21 +4,44 @@ import { useNavigate } from "react-router-dom";
 function Users({ usersList, setUsersList, entranceExit, setEntranceExit }) {
   const navigate = useNavigate();
 
-  function navegar() {
+  function handleGoBack() {
     setEntranceExit(false);
 
     setTimeout(() => {
       navigate("/");
+      setUsersList([]);
     }, 900);
   }
   console.log(usersList);
   return (
     <div className="container-main">
       <div className="container-background container-users">
-        <div className={` ${entranceExit ? "slide-in-bottom" : "slide-out-bottom"}`}>
-          <h1>Usuários Encontrados</h1>
-          <button onClick={navegar}>Voltar</button>
+        <div className={` users ${entranceExit ? "slide-in-bottom" : "slide-out-bottom"}`}>
+          <h1>Usuários</h1>
+
+          {usersList.map((user) => (
+            <div className='user-card'>
+              <img src={user.avatar_url
+              } alt="avatar" />
+
+              <div className='info-user'>
+                <h3>
+                  {user.login}
+                </h3>
+                <a
+                  href={user.html_url}
+                  target="_blank"
+                >
+                  <div className='visit-btn'>
+                    Visitar
+                  </div>
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
+
+        <button onClick={handleGoBack}>Voltar</button>
       </div>
     </div>
   )
